ba6ea686d30f0aeb7711e1d7efd675cd376b7122
Manuel <leyva4manuel@gmail.com>
2023-05-11 08:48:36 -0700

    update img

--- src/Components/AboutMeComponent/AboutMeComponent.js
+++ src/Components/AboutMeComponent/AboutMeComponent.js
@@ -14,7 +14,7 @@ export default function AboutMeComponent() {
 
         <Row>
             <Col>
-
+                
             </Col>
             <Col>
             
--- src/Components/PortfolioComponent/PortfolioComponent.js
+++ src/Components/PortfolioComponent/PortfolioComponent.js
@@ -9,19 +9,25 @@ export default function PortfolioComponent() {
   return (
     <div className='brImg'>
       <NavbarComponent />
+
+      <Row className='pt-5'>
+        <Col lg={12}className='underCuz'>
+          <h1 className='text-center'>Under Construction</h1>
+        </Col>
+      </Row>
       <Row className='mPic'>
         <Col lg={4} className='movePicture'>
           <img className='customizeProfilePic' src={ProfilePicture} height='350px' alt='Picture of Manuel' />
         </Col>
         <Col lg={8} className='customizeText'>
           <h1 className='text-white'>Hey, I'm Manuel</h1>
-          <h5 className='text-white'>Jr. Software Engineer </h5>
+          <h5 className='text-white'>I'm a Jr. Front-End engineer based in Stockton, CA. I spcialize <br /> in building   exceptional websites, applications, and everything <br /> in between. </h5>
         </Col>
       </Row>
 
       <Row>
         <Col className='aboutMeCuz'>
-        <AboutMeComponent />
+        {/* <AboutMeComponent /> */}
         </Col>
       </Row>
     </div>
